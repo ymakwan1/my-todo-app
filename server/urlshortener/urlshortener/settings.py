25e5697cde12d954d773b378b395afd6923dbd13
ymakwan1 <ymakwan1@binghamton.edu>
2024-07-19 20:55:43 -0400

    fix

--- server/urlshortener/urlshortener/settings.py
+++ server/urlshortener/urlshortener/settings.py
@@ -29,9 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+
 
 # Application definition
 
